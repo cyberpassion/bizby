6a074e5efe2fdc041a072fd7ec9fc6ddfdb76aa3
Ravi Sharma <ravi26march@gmail.com>
2025-11-20 15:17:14 +0530

    Update 2025_11_20_093936_create_cyp_consultation_table.php

--- Modules/Consultation/database/migrations/2025_11_20_093936_create_cyp_consultation_table.php
+++ Modules/Consultation/database/migrations/2025_11_20_093936_create_cyp_consultation_table.php
@@ -8,44 +8,42 @@ return new class extends Migration
 {
     public function up(): void
     {
-        if (!Schema::hasTable('cyp_consultation')) {
-            Schema::create('cyp_consultation', function (Blueprint $table) {
-                $table->id('consultation_id');
-                $table->unsignedBigInteger('consultation_group_id')->nullable();
-                $table->date('date')->nullable();
-                $table->dateTime('datetime')->nullable();
-                $table->date('consultation_date')->nullable();
-                $table->time('consultation_time')->nullable();
-                $table->unsignedBigInteger('day_token_id')->nullable();
-                $table->string('consultation_through')->nullable();
-                $table->unsignedBigInteger('consultation_with')->nullable();
-                $table->text('consultation_for')->nullable();
-                $table->text('consultation_for_detail')->nullable();
-                $table->unsignedBigInteger('patient_id')->nullable();
-                $table->string('patient_name')->nullable();
-                $table->string('guardian_type')->nullable();
-                $table->string('guardian_name')->nullable();
-                $table->string('father_name')->nullable();
-                $table->string('phone_number')->nullable();
-                $table->string('gender')->nullable();
-                $table->string('email')->nullable();
-                $table->unsignedTinyInteger('age')->nullable();
-                $table->string('permanent_address')->nullable();
-                $table->string('aadhar_number')->nullable();
-                $table->string('consultation_type')->nullable();
-                $table->decimal('consultation_fee', 10, 2)->nullable();
-                $table->decimal('consultation_extra_fee', 10, 2)->nullable();
-                $table->string('referred_by')->nullable();
-                $table->string('referred_to')->nullable();
-                $table->text('remark')->nullable();
-                $table->string('expected_next_consultation_after')->nullable();
-                $table->date('next_date')->nullable();
-                $table->unsignedBigInteger('thread_parent')->nullable();
-                $table->unsignedTinyInteger('status')->default(1);
-                $table->unsignedBigInteger('client_id')->nullable();
-                $table->timestamps();
-            });
-        }
+        Schema::create('cyp_consultation', function (Blueprint $table) {
+            $table->id('consultation_id');
+            $table->unsignedBigInteger('consultation_group_id')->nullable();
+            $table->date('date')->nullable();
+            $table->dateTime('datetime')->nullable();
+            $table->date('consultation_date')->nullable();
+            $table->time('consultation_time')->nullable();
+            $table->unsignedBigInteger('day_token_id')->nullable();
+            $table->string('consultation_through')->nullable();
+            $table->unsignedBigInteger('consultation_with')->nullable();
+            $table->text('consultation_for')->nullable();
+            $table->text('consultation_for_detail')->nullable();
+            $table->unsignedBigInteger('patient_id')->nullable();
+            $table->string('patient_name')->nullable();
+            $table->string('guardian_type')->nullable();
+            $table->string('guardian_name')->nullable();
+            $table->string('father_name')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('email')->nullable();
+            $table->unsignedTinyInteger('age')->nullable();
+            $table->string('permanent_address')->nullable();
+            $table->string('aadhar_number')->nullable();
+            $table->string('consultation_type')->nullable();
+            $table->decimal('consultation_fee', 10, 2)->nullable();
+            $table->decimal('consultation_extra_fee', 10, 2)->nullable();
+            $table->string('referred_by')->nullable();
+            $table->string('referred_to')->nullable();
+            $table->text('remark')->nullable();
+            $table->string('expected_next_consultation_after')->nullable();
+            $table->date('next_date')->nullable();
+            $table->unsignedBigInteger('thread_parent')->nullable();
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->timestamps();
+        });
     }
 
     public function down(): void
