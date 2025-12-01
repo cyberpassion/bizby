dc4686d519d9200d8e4024d35607cb807c6d24c6
Ravi Sharma <ravi26march@gmail.com>
2025-12-01 12:25:44 +0530

    updates

---
+++ Modules/Registration/lookups.php
@@ -0,0 +1,35 @@
+<?php
+$pg = 'registration';
+$commonSettingsRoute = '/settings';
+
+return [
+	'menuItem-registration' => [
+		'admin'	=>	[
+			'parent'		=>	[
+				$pg	=>	'#',
+			],
+			'child'		=>	[
+				$pg	=>	[
+					['Add New'		=> "/{$pg}/create"],
+	                ['View List'	=> "/{$pg}/list"],
+    	            ['Report'		=> "/{$pg}/report"],
+        	        ['Settings'		=> "/{$pg}/settings"],
+				],
+			],
+		],
+	],
+    'sidebar-menu' => [
+        [
+            'title' => ucfirst($pg),
+            'href' => "/{$pg}",
+            'items' => [
+                ['title' => 'Home', 'href' => "/module/{$pg}/home"],
+				['title' => 'Add New', 'href' => "/module/{$pg}/new"],
+                ['title' => 'View List', 'href' => "/module/{$pg}/list"],
+                ['title' => 'Report', 'href' => "/module/{$pg}/report"],
+                ['title' => 'Settings', 'href' => "/module/{$pg}/settings"],
+            ],
+        ],
+    ]
+
+];
