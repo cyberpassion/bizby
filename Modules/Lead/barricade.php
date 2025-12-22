e9e85d1608cad41050d4e55b175339808c9e59f4
Ravi Sharma <ravi26march@gmail.com>
2025-12-22 22:28:30 +0530

    update

--- Modules/Lead/barricade.php
+++ Modules/Lead/barricade.php
@@ -1,6 +1,21 @@
 <?php
 return [
-    'path.lead.create' => [],
+    'path.lead.create' => [
+		[
+            'type'     => 'exists',
+            'resource' => 'terms',
+            'filter'   => ['module' => 'lead', 'group' => 'lead-categories'],
+            'message'  => 'Please create at least one category before proceeding.',
+            'action'   => '/module/shared/terms/lead/lead-categories',
+        ],
+		[
+            'type'     => 'exists',
+            'resource' => 'employees',
+            'filter'   => ['status'=>true],
+            'message'  => 'Please add employee before adding proceeding.',
+            'action'   => '/module/employee/new',
+        ]
+	],
 	'path.lead.read' => [],
 	'path.lead.update' => [],
 	'path.lead.delete' => [],
--- Modules/Patient/barricade.php
+++ Modules/Patient/barricade.php
@@ -1,6 +1,14 @@
 <?php
 return [
-    'path.patient.create' => [],
+    'path.patient.create' => [
+		[
+            'type'     => 'exists',
+            'resource' => 'employees',
+            'filter'   => ['status'=>true],
+            'message'  => 'Please add employee before adding proceeding.',
+            'action'   => '/module/employee/new',
+        ]
+	],
 	'path.patient.read' => [],
 	'path.patient.update' => [],
 	'path.patient.delete' => [],
