3bce907c2839d62a1ac3850a3ba352a69ade4f00
Ravi Sharma <ravi26march@gmail.com>
2025-12-22 13:01:08 +0530

    updtes

--- Modules/Consultation/barricade.php
+++ Modules/Consultation/barricade.php
@@ -6,7 +6,7 @@ return [
             'resource' => 'employees',
             'filter'   => ['status'=>true],
             'message'  => 'Please add employee before adding consultation.',
-            'action'   => '/module/employee/create',
+            'action'   => '/module/employee/new',
         ]
     ]
 
