ebb1719d6309d837aed329a910569fe95a224f86
Ravi Sharma <ravi26march@gmail.com>
2026-04-24 16:56:00 +0530

    update

--- Modules/Center/database/migrations/tenant/001_create_center_table.php
+++ Modules/Center/database/migrations/tenant/001_create_center_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->text('location');
             $table->string('contact');
             $table->integer('capacity')->nullable();
-			$table->text('remark')->nullable();
         });
     }
 
